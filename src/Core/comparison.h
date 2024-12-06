647ff477a7aec115f295d7bd6ffcabc9251c2da5
Maestro <skhvalchev@gmail.com>
2024-12-06 16:45:06 +0300

    Make all comparison

---
+++ src/Core/comparison.h
@@ -0,0 +1,4 @@
+#include "../s21_decimal.h"
+#include <stdint.h>
+
+int s21_sign_get(s21_decimal decimal1, int index, int numBits);
---
+++ src/Core/is_equal.c
@@ -0,0 +1,131 @@
+#include "comparison.h"
+#include "../s21_decimal.h"
+COMPARISON_STATUS s21_is_equal(s21_decimal decimal1, s21_decimal decimal2)
+{
+	COMPARISON_STATUS res = COMPARISON_FALSE;
+	if (get_sign_number(decimal1) == get_sign_number(decimal2))
+	{
+		if (get_bits_16_23(decimal1) == get_bits_16_23(decimal2))
+		{
+			if (decimal1.bits[0] == decimal2.bits[0] && decimal1.bits[1] == decimal2.bits[1] && decimal1.bits[2] == decimal2.bits[2])
+			{
+				res = COMPARISON_TRUE;
+			}
+		}
+	}
+	return res;
+}
+COMPARISON_STATUS s21_is_not_equal(s21_decimal decimal1, s21_decimal decimal2)
+{
+	COMPARISON_STATUS res;
+	if (s21_is_equal(decimal1, decimal2))
+	{
+		res = COMPARISON_FALSE;
+	}
+	else
+	{
+		res = COMPARISON_TRUE;
+	}
+	return res;
+}
+
+COMPARISON_STATUS s21_is_greater(s21_decimal decimal1, s21_decimal decimal2)
+{
+	COMPARISON_STATUS res = COMPARISON_FALSE;
+
+	if (get_sign_number(decimal1) < get_sign_number(decimal2))
+	{
+		res = COMPARISON_TRUE; // Положительное больше отрицательного
+	}
+	else if (get_sign_number(decimal1) > get_sign_number(decimal2))
+	{
+		res = COMPARISON_FALSE; // Отрицательное меньше положительного
+	}
+	else if (get_bits_16_23(decimal1) != get_bits_16_23(decimal2))
+	{
+		res = get_bits_16_23(decimal1) > get_bits_16_23(decimal2) ? COMPARISON_TRUE : COMPARISON_FALSE;
+	}
+	else
+	{
+		bool flag = COMPARISON_TRUE;
+		for (int i = 2; i >= 0 && flag; --i)
+		{
+			if (decimal1.bits[i] > decimal2.bits[i])
+			{
+				res = COMPARISON_TRUE;
+				flag = COMPARISON_FALSE; // Прерываем цикл, так как результат уже определён
+			}
+			else if (decimal1.bits[i] < decimal2.bits[i])
+			{
+				res = COMPARISON_FALSE;
+				flag = COMPARISON_FALSE; // Прерываем цикл, так как результат уже определён
+			}
+		}
+	}
+	return res;
+}
+int s21_sign_get(s21_decimal decimal1, int index, int numBits)
+{
+	return (decimal1.bits[numBits] >> index) & 1;
+}
+COMPARISON_STATUS s21_is_greater_or_equal(s21_decimal decimal1, s21_decimal decimal2)
+{
+	COMPARISON_STATUS res = COMPARISON_FALSE;
+	if (s21_is_equal(decimal1, decimal2) || s21_is_greater(decimal1, decimal2))
+	{
+		res = COMPARISON_TRUE;
+	}
+	return res;
+}
+COMPARISON_STATUS s21_is_less(s21_decimal decimal1, s21_decimal decimal2)
+{
+	COMPARISON_STATUS res = COMPARISON_FALSE;
+	if (!s21_is_greater_or_equal(decimal1, decimal2))
+	{
+		res = COMPARISON_TRUE;
+	}
+	return res;
+}
+COMPARISON_STATUS s21_is_less_or_equal(s21_decimal decimal1, s21_decimal decimal2)
+{
+	COMPARISON_STATUS res = COMPARISON_FALSE;
+	if (!s21_is_greater(decimal1, decimal2))
+	{
+		res = COMPARISON_TRUE;
+	}
+	return res;
+}
+// int main()
+// {
+// 	// Примеры чисел для тестов
+// 	s21_decimal decimal1 = {{1, 0, 0, 0}};				// 1
+// 	s21_decimal decimal2 = {{2, 0, 0, 0}};				// 2
+// 	s21_decimal decimal3 = {{1, 0, 0, 0}};				// 1
+// 	s21_decimal decimal4 = {{1, 0, 0, 0x80000000}}; // -1
+
+// 	// Тестирование s21_is_equal
+// 	printf("s21_is_equal(decimal1, decimal2): %d\n", s21_is_equal(decimal1, decimal2));
+// 	printf("s21_is_equal(decimal1, decimal3): %d\n", s21_is_equal(decimal1, decimal3));
+// 	printf("s21_is_equal(decimal1, decimal4): %d\n", s21_is_equal(decimal1, decimal4));
+// 	// Тестирование s21_is_not_equal
+// 	printf("s21_is_not_equal(decimal1, decimal2): %d\n", s21_is_not_equal(decimal1, decimal2));
+// 	printf("s21_is_not_equal(decimal1, decimal3): %d\n", s21_is_not_equal(decimal1, decimal3));
+
+// 	// Тестирование s21_is_greater
+// 	printf("s21_is_greater(decimal1, decimal2): %d\n", s21_is_greater(decimal1, decimal2));
+// 	printf("s21_is_greater(decimal2, decimal1): %d\n", s21_is_greater(decimal2, decimal1));
+
+// 	// Тестирование s21_is_less
+// 	printf("s21_is_less(decimal1, decimal2): %d\n", s21_is_less(decimal1, decimal2));
+// 	printf("s21_is_less(decimal2, decimal1): %d\n", s21_is_less(decimal2, decimal1));
+
+// 	// Тестирование s21_is_greater_or_equal
+// 	printf("s21_is_greater_or_equal(decimal1, decimal2): %d\n", s21_is_greater_or_equal(decimal1, decimal2));
+// 	printf("s21_is_greater_or_equal(decimal1, decimal3): %d\n", s21_is_greater_or_equal(decimal1, decimal3));
+
+// 	// Тестирование s21_is_less_or_equal
+// 	printf("s21_is_less_or_equal(decimal1, decimal2): %d\n", s21_is_less_or_equal(decimal1, decimal2));
+// 	printf("s21_is_less_or_equal(decimal1, decimal3): %d\n", s21_is_less_or_equal(decimal1, decimal3));
+
+// 	return 0;
+// }
